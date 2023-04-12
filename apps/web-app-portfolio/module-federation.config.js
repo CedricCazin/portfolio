acff399995343107899f754120f94d307e7c686d
CedricCazin <cedric.cazin@bearingpoint.com>
2023-04-13 00:17:27 +0200

    feat: add remote app portfolio

---
+++ apps/web-app-portfolio/module-federation.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'web-app-portfolio',
+  exposes: {
+    './Module': 'apps/web-app-portfolio/src/app/remote-entry/entry.module.ts',
+  },
+};
---
+++ apps/web-app-portfolio/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-web-app-portfolio-entry',
+  template: `<portfolio-nx-welcome></portfolio-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
