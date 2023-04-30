f830b3791b1c8c4a65d659dcbe813107c71b3de5
CedricCazin <cedric.cazin@bearingpoint.com>
2023-05-01 01:04:50 +0200

    feat: add hexagons background

--- apps/web-app/src/app/app.component.ts
+++ apps/web-app/src/app/app.component.ts
@@ -1,50 +1,70 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 declare class FinisherHeader {
-  constructor(i: any);
+    constructor(i: any);
 }
 
 @Component({
-  selector: 'portfolio-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'portfolio-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
-  title = 'web-app';
+    title = 'web-app';
 
-  ngAfterViewInit(): void {
-    new FinisherHeader({
-      count: 6,
-      size: {
-        min: 1100,
-        max: 1300,
-        pulse: 0,
-      },
-      speed: {
-        x: {
-          min: 0.1,
-          max: 0.3,
-        },
-        y: {
-          min: 0.1,
-          max: 0.3,
-        },
-      },
-      colors: {
-        background: '#9138e5',
-        particles: ['#6bd6ff', '#ffcb57', '#ff333d'],
-      },
-      blending: 'overlay',
-      opacity: {
-        center: 1,
-        edge: 0.1,
-      },
-      skew: -2,
-      shapes: ['c'],
-    });
-  }
+    cursorLeft = '';
+    cursorTop = '';
+    cursorOpacity = '';
 
-  onClick() {
-    console.log('click');
-  }
+    ngAfterViewInit(): void {
+        this.initHeader();
+    }
+
+    private initHeader() {
+        new FinisherHeader({
+            count: 6,
+            size: {
+                min: 1100,
+                max: 1300,
+                pulse: 0,
+            },
+            speed: {
+                x: {
+                    min: 0.1,
+                    max: 0.3,
+                },
+                y: {
+                    min: 0.1,
+                    max: 0.3,
+                },
+            },
+            colors: {
+                background: '#9138e5',
+                particles: ['#6bd6ff', '#ffcb57', '#ff333d'],
+            },
+            blending: 'overlay',
+            opacity: {
+                center: 1,
+                edge: 0.1,
+            },
+            skew: -2,
+            shapes: ['c'],
+        });
+    }
+
+    onClick() {
+        console.log('click');
+    }
+
+    // --
+
+    @HostListener('mousemove', ['$event'])
+    onMousemove(event: any) {
+        const x = event.pageX;
+        const y = event.pageY;
+
+        this.cursorLeft = (x - 175) + "px";
+        this.cursorTop = (y - 175) + "px";
+        this.cursorOpacity = '1';
+    }
 }
