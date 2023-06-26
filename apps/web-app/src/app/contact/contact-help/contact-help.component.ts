e04a21d0d56d24ac78f5fdd52500b9006c7c8f54
CedricCazin <cedric.cazin@bearingpoint.com>
2023-06-26 11:35:54 +0200

    feat: add contact

---
+++ apps/web-app/src/app/contact/contact-help/contact-help.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-contact-help',
+  templateUrl: './contact-help.component.html',
+  styleUrls: ['./contact-help.component.scss'],
+})
+export class ContactHelpComponent { }
