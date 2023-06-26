9a416bf81291d524cb09b3eaa7f063158245fffb
CedricCazin <cedric.cazin@bearingpoint.com>
2023-06-26 11:35:54 +0200

    feat: add about

---
+++ apps/web-app/src/app/about/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'portfolio-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+})
+export class AboutComponent {
+  constructor(private router: Router) { }
+}
