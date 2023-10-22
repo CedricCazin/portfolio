5026d238fd861ea482af05f386ea5797bae12ff5
CedricCazin <cedric.cazin@bearingpoint.com>
2023-10-22 20:19:52 +0200

    test(web-app,web-app-games): add required input

--- apps/web-app/src/app/shared/floating-skill/floating-skill.component.spec.ts
+++ apps/web-app/src/app/shared/floating-skill/floating-skill.component.spec.ts
@@ -12,6 +12,15 @@ describe('FloatingSkillComponent', () => {
 
         fixture = TestBed.createComponent(FloatingSkillComponent);
         component = fixture.componentInstance;
+        component.skill = {
+            name: 'Angular',
+            description:
+                'Angular is a development platform, built on TypeScript. As a platform, Angular includes: A component-based framework for building scalable web applications. A collection of well-integrated libraries that cover a wide variety of features, including routing, forms management, client-server communication, and more.',
+            borderColor: 'gold',
+            progress: 100,
+            url: 'https://angular.io/',
+            image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg',
+        };
         fixture.detectChanges();
     });
 
