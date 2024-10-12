e17bfd0db2b0de857104f08f751fdf4d049f9ce1
CedricCazin <cedric.cazin@bearingpoint.com>
2024-10-12 20:03:35 +0200

    lint(repo): update commit lint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,52 +1,65 @@
-const {
-    utils: { getProjects },
-} = require('@commitlint/config-nx-scopes');
-
 // prettier-ignore
 const types = [
-    { value: 'feat',            name: 'feat:            (minor) A new feature' },
-    { value: 'deps',            name: 'deps:            (minor) Anything related to dependencies' },
-    { value: 'revert',          name: 'revert:          (minor) Reverts a previous commit' },
-    
-    { value: 'fix',             name: 'fix:             (patch) A bug fix' },
-    { value: 'perf',            name: 'perf:            (patch) A code change that improves performance' },
-    { value: 'refactor',        name: 'refactor:        (patch) A code change that neither fixes a bug or adds a feature' },
-    { value: 'style',           name: 'style:           (patch) Markup, white-space, formatting, missing semi-colons...' },
+    { value: 'major',           name: 'major:           (major) Create a release commit' },
 
-    { value: 'chore',           name: 'chore:           (none) Build process or auxiliary tool changes' },
-    { value: 'docs',            name: 'docs:            (none) Documentation only changes' },
-    { value: 'test',            name: 'test:            (none) Adding missing tests' },
-    { value: 'lint',            name: 'lint:            (none) Lint fixes' },
-    { value: 'config',          name: 'config:          (none) anything related to configuration' },
-    { value: 'wip',             name: 'wip:             (none) Work in progress' },
+    { value: 'feat',            name: 'feat:            (minor) ✨ A new feature' },
+    { value: 'minor',           name: 'minor:           (minor) Create a release commit' },
+
+    { value: 'fix',             name: 'fix:             (patch) 🐛 A bug fix' },
+    { value: 'deps',            name: 'deps:            (patch) 📎 Anything related to dependencies' },
+    { value: 'perf',            name: 'perf:            (patch) ⚡ A code change that improves performance' },
+    { value: 'refactor',        name: 'refactor:        (patch) ♻️  A code change that neither fixes a bug or adds a feature' },
+    { value: 'style',           name: 'style:           (patch) 🎨 Anything to scss, css, themes...' },
+    { value: 'revert',          name: 'revert:          (patch) ◀️  Reverts a previous commit' },
+    { value: 'patch',           name: 'patch:           (patch) Create a release commit' },
+
+    { value: 'chore',           name: 'chore:           (none) 🧹 Cleaning the house' },
+    { value: 'docs',            name: 'docs:            (none) 📝 Documentation changes' },
+    { value: 'test',            name: 'test:            (none) 🧪 Anything related to tests' },
+    { value: 'lint',            name: 'lint:            (none) 📑 Markup, white-space, formatting, missing semi-colons...' },
+    { value: 'build',           name: 'build:           (none) 🤖 Anything related to build, tools or dev-env' },
+    { value: 'config',          name: 'config:          (none) 🔧 Anything related to configuration' },
+    { value: 'wip',             name: 'wip:             (none) 🚧 Work in progress' },
 ];
 
 // prettier-ignore
 const scopes = [
+    { value: 'repo',            name: 'repo:            anything related to managing the repo itself'},
     { value: 'ci',              name: 'ci:              anything ci specific' },
     { value: 'deploy',          name: 'deploy:          anything deployment specific' },
-    { value: 'repo',            name: 'repo:            anything related to managing the repo itself'},
+    { value: 'docs',            name: 'docs:            docs folder' },
     { value: 'tools',           name: 'tools:           anything related to tools'},
     // nx projects will be added in the cli
 ];
 
 /** @type {import('cz-git').CommitizenGitOptions} */
-module.exports = {
-    // commitlint options
-    extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
-    ignores: [(message) => message.includes('WIP'), (message) => message.includes('[skip ci]')],
-    utils: { getProjects },
-    rules: {
-        'type-enum': [2, 'always', Object.values(types).map((s) => s.value)],
-        'scope-empty': [2, 'never'],
-        'scope-enum': (ctx) =>
-            getProjects(ctx).then((packages) => [1, 'always', ['all', ...scopes.map((s) => s.value), packages]]),
-    },
+async function getConfig() {
+    const {
+        default: {
+            utils: { getProjects },
+        },
+    } = await import('@commitlint/config-nx-scopes');
 
-    // cz-git, czg options
-    types,
-    enableMultipleScopes: true,
-    allowCustomScopes: true,
-    allowEmptyScopes: false,
-    scopes: (ctx) => getProjects(ctx).then((packages) => [...scopes, ...packages]),
-};
+    const projects = await getProjects();
+    projects.map((p) => scopes.push({ value: p, name: `${p}: anything related to ${p}` }));
+
+    return {
+        // commitlint options
+        extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
+        ignores: [(message) => message.includes('WIP'), (message) => message.includes('[skip ci]')],
+        rules: {
+            'type-enum': [2, 'always', Object.values(types).map((s) => s.value)],
+            'scope-empty': [2, 'never'],
+            'scope-enum': [1, 'always', ['all', ...scopes.map((s) => s.value), projects]],
+        },
+
+        // cz-git, czg options
+        types,
+        enableMultipleScopes: true,
+        allowCustomScopes: true,
+        allowEmptyScopes: true,
+        scopes: [...scopes, ...projects],
+    };
+}
+
+module.exports = getConfig();
